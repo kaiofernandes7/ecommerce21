6abbacee7e918da7d9256039739644dd3bc298ed
kaiofernandes7 <kaioinformatica7@gmail.com>
2024-11-25 08:57:30 -0300

    crindo barra de pesquisa dinâmica

--- header.php
+++ header.php
@@ -6,34 +6,54 @@
 
   <!-- Inicio menu navegação -->
   <nav class="menu">
-  <ul id="MenuItens" class="menu-list">
-    <li class="menu-item"><a href="index.php" class="menu-link" title="">Início</a></li>
-    <li class="menu-item"><a href="produtos.php" class="menu-link" title="">Produtos</a></li>
-    <li class="menu-item"><a href="conta.php" class="menu-link" title="">Minha conta</a></li>
+    <ul id="MenuItens" class="menu-list">
+      <li class="menu-item"><a href="index.php" class="menu-link">Início</a></li>
+      <li class="menu-item"><a href="produtos.php" class="menu-link">Produtos</a></li>
+      <li class="menu-item"><a href="conta.php" class="menu-link">Minha conta</a></li>
 
-    <li class="menu-item">
-      <form action="categoria.php" method="GET" class="category-form">
-        <label for="categoria" class="category-label"></label>
-        <select name="id_categoria" id="categoria" class="category-select" onchange="this.form.submit()">
-          <option value="" disabled selected>Categoria</option>
-          <?php
-          // Seleciona as categorias do banco de dados
-          $sql = "SELECT * FROM categorias";
-          $stmt = $pdo->query($sql);
+      <li class="menu-item">
+        <form action="categoria.php" method="GET" class="category-form">
+          <label for="categoria" class="category-label"></label>
+          <select name="id_categoria" id="categoria" class="category-select" onchange="this.form.submit()">
+            <option value="" disabled selected>Categoria</option>
+            <?php
+            // Seleciona as categorias do banco de dados
+            $sql = "SELECT * FROM categorias";
+            $stmt = $pdo->query($sql);
+            while ($categoria = $stmt->fetch(PDO::FETCH_ASSOC)) {
+              echo '<option value="' . $categoria['id'] . '">' . htmlspecialchars($categoria['nome']) . '</option>';
+            }
+            ?>
+          </select>
+        </form>
+      </li>
+      
+      <li class="menu-item">
+        <form action="produtos.php" method="GET" class="search-form">
+          <input 
+            type="text" 
+            name="busca" 
+            id="search-input"
+            placeholder="Pesquisar produtos..." 
+            class="search-input" 
+            value="<?php echo isset($_GET['busca']) ? htmlspecialchars($_GET['busca']) : ''; ?>"
+            onkeyup="showSuggestions()"
+          >
+          <button type="submit" class="search-button">Buscar</button>
+        </form>
 
-          while ($categoria = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo '<option value="' . $categoria['id'] . '">' . htmlspecialchars($categoria['nome']) . '</option>';
-          }
-          ?>
-        </select>
-      </form>
-    </li>
-  </ul>
-</nav>
-
+        <!-- Container para sugestões de pesquisa -->
+        <div id="suggestions-container" class="suggestions-container"></div>
+      </li>
+    </ul>
+  </nav>
   <!-- fim menu navegação -->
 
-  <a href="carrinho.php" title><img src="assets/img/carrinho.png" alt="" width="30px" height="30px " class="cor-imagem"></a>
-  <img src="assets/img/menu.png" alt="" class="menu-celular" onclick="menucelular()">
+  <a href="carrinho.php" class="carrinho-link">
+    <img src="assets/img/carrinho.png" alt="Carrinho" width="30px" height="30px" class="cor-imagem">
+  </a>
+
+  <img src="assets/img/menu.png" alt="Menu" class="menu-celular" onclick="menucelular()">
 </div>
 <!-- fim navegação -->
+
--- produtos.php
+++ produtos.php
@@ -3,8 +3,18 @@ session_start();
 require 'conexao2.php'; 
 
 
+$termo = isset($_GET['busca']) ? trim($_GET['busca']) : '';
+
+
 $sql = "SELECT id, nome, preco, imagem FROM produtos";
+if ($termo) {
+    $sql .= " WHERE nome LIKE :termo";
+}
 $stmt = $pdo->prepare($sql);
+
+if ($termo) {
+    $stmt->bindValue(':termo', '%' . $termo . '%');
+}
 $stmt->execute();
 $produtos = $stmt->fetchAll();
 ?>
@@ -25,20 +35,23 @@ $produtos = $stmt->fetchAll();
 
         <h1>Produtos</h1>
 
-        <div class="corpo-categorias">
-            <div class="linha">
-                <?php foreach ($produtos as $produto): ?>
-                <div class="col-4">
-                    <img src="<?php echo htmlspecialchars($produto['imagem']); ?>" alt="<?php echo htmlspecialchars($produto['nome']); ?>" />
-                    <h4><?php echo htmlspecialchars($produto['nome']); ?></h4>
-                    <p>R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></p>
-                    <a href="ver-produto.php?id=<?php echo $produto['id']; ?>" class="btn">Ver Detalhes</a>
-                </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-
         
+        <?php if (empty($produtos)): ?>
+            <p>Nenhum produto encontrado para sua busca.</p>
+        <?php else: ?>
+            <div class="corpo-categorias">
+                <div class="linha">
+                    <?php foreach ($produtos as $produto): ?>
+                    <div class="col-4">
+                        <img src="<?php echo htmlspecialchars($produto['imagem']); ?>" alt="<?php echo htmlspecialchars($produto['nome']); ?>" />
+                        <h4><?php echo htmlspecialchars($produto['nome']); ?></h4>
+                        <p>R$ <?php echo number_format($produto['preco'], 2, ',', '.'); ?></p>
+                        <a href="ver-produto.php?id=<?php echo $produto['id']; ?>" class="btn">Ver Detalhes</a>
+                    </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endif; ?>
     </div>
     <?php include 'footer.php'; ?>
 
